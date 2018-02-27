521fbf515a201ef49b92c2407fc4711db1c3c03f
Dennis Johnson <dzjohnson@godaddy.com>
2018-02-26 22:13:51 -0800

    added pathfinder movement

--- path.utils.js
+++ path.utils.js
@@ -16,3 +16,41 @@ function reversePathArray(path){
     console.log("Path before reversal: ", JSON.stringify(path));
     return [...path].reverse()
 }
+
+
+function moveToByPatfinderPath(creep, path){
+    if(path.path){path = path.path}
+    let minRange, minRangeIndex;
+    let j = -1;
+    for (let i = path.length; i >=0 ; i--){
+        j++;
+        let currentSpot = new RoomPosition(path[i].x, path[i].y, path[i].roomName);
+        let deltas = getDeltas(creep.pos, currentSpot);
+        let rangeToCurrentSpot = getRangeFromDeltas(deltas);
+        if (rangeToCurrentSpot === 1){
+            moveOneStep(creep, deltas);
+        }
+    }
+}
+
+function getDeltas(startPos, destPos){
+    if(startPos.roomName !== destPos.roomName){return {"x": 100, "y": 100};}
+    return {"x": destPos.x - startPos.x, "y": destPos.y - startPos.y}
+}
+function getRangeFromDeltas(deltas){
+    return max(Math.abs(deltas.x), Math.abs(deltas.y));
+}
+
+function moveOneStep(creep, deltas){
+    return creep.move(getDirectionFromDeltas(deltas));
+
+}
+function getDirectionFromDeltas(deltas){
+    let direction = 0; //TOP
+    if (deltas.y < 0){direction = direction + TOP;}
+    if (deltas.y === 0){}
+    if (deltas.y === 0){}
+    if (deltas.y === 0){}
+    if (deltas.y === 0){}
+    if (deltas.y === 0){}
+}
